73c15cd5dc972c415205cd81f3d27b47149e3dca
KareemMAX <kreem.morsy@hotmail.com>
2019-07-11 05:52:31 +0200

    Add background

--- timer.js
+++ timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jule 26, 2019 00:00:00").getTime();
+var countDownDate = new Date("July 26, 2019 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
